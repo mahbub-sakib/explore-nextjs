4804500c58ad1dbaacc7780f124d3f7b6217abb2
mahbub-sakib <mahbub.sakib418@gmail.com>
2024-10-29 16:47:31 +0600

    m76 v10 done

--- src/middleware.js
+++ src/middleware.js
@@ -7,14 +7,36 @@ import { NextResponse } from "next/server"
 // export const middleware = (request) => {
 //     return NextResponse.redirect(new URL('/contacts', request.url))
 // }
+// export const middleware = (request) => {
+//     if (request.nextUrl.pathname.startsWith('/about')) {
+//         return NextResponse.redirect(new URL('/contacts', request.url))
+//     }
+// }
+
+// user authentication
+// const user = true;
+
+// export const middleware = (request) => {
+//     if(!user){
+//         return NextResponse.redirect(new URL('/login', request.url))
+//     }    
+
+//     return NextResponse.next()
+// }
+
+// authentication by cookies
+const cook = 'my-cooki'
+
 export const middleware = (request) => {
-    if (request.nextUrl.pathname.startsWith('/about')) {
-        return NextResponse.redirect(new URL('/contacts', request.url))
+    const cookies = request.cookies.get('token');
+    if(!cookies || cookies.value !== cook){
+        return NextResponse.redirect(new URL('/login', request.url))
     }
+
+    return NextResponse.next()
 }
 
 
-
-// export const config = {
-//     matcher: ['/about', '/services']
-// }
+export const config = {
+    matcher: ['/dashboard', '/services']
+}
